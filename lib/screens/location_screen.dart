40f2f2e740d0706f513adc289c2204bd5b334df1
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-01-23 21:41:35 +0900

    locationWeatherを表示してみる

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -12,6 +12,13 @@ class LocationScreen extends StatefulWidget {
 }
 
 class _LocationScreenState extends State<LocationScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    print(widget.locationWeather);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
