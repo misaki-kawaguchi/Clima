82b9fb5ecb180a456b8530850d3a5a7b118f126a
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-01-23 14:14:38 +0900

    非同期メソッド追加

--- scratch.dart
+++ scratch.dart
@@ -17,10 +17,10 @@ void task1() {
 
 void task2()  {
   Duration threeSeconds = Duration(seconds: 3);
-  sleep(threeSeconds);
-
-  String result = 'task 2 data';
-  print('Task 2 complete');
+  Future.delayed(threeSeconds, (){
+    String result = 'task 2 data';
+    print('Task 2 complete');
+  });
 }
 
 void task3() {
